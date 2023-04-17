d36d0bbacc5df4cd8cebd011656f39ad19edd2ee
Peby1407 <127780305+Peby1407@users.noreply.github.com>
2023-04-17 14:34:08 -0600

    All files Uploaded

---
+++ PyBank/main.py
@@ -0,0 +1,61 @@
+# Creating PyBank code
+#Falta transformar archivo a text y print in the terminal repository
+#importing module os and csv
+import os, csv
+with open("text_file.txt", "a") as f:
+    #csvpath = os.path.join("Resources","budget_data.csv") 
+    #opening file
+    with open('budget_data.csv', encoding='UTF-8') as csvfile:
+    #with open(csvpath, encoding='UTF-8') as csvfile:
+        csv_reader = csv.reader(csvfile)
+        #skipping first row
+        next(csvfile)
+        #defining variables
+        total_months = 0
+        sum_months = 0
+        list_months_total = []
+        list_of_differences = []
+        list_of_months = []
+        profit_change=[]
+        
+        for m in csv_reader:
+            months=m[0]
+            amounts=m[1]
+            #counting total amount of months
+            total_months+= 1
+            #calculating the total sum for the entire period
+            sum_months = sum_months + float(m[1])
+            #creating list of all values
+            list_months_total.append(amounts)
+            #creating list for all dates
+            list_of_months.append(months)
+        #changing the values list to integers            
+        for i in range(0, len(list_months_total)):
+            list_months_total[i] = int(list_months_total[i])
+        print('Financial Analysis', file = f)
+        print('--------------------------------------', file = f)
+        print("Total Months: ", total_months, file = f)
+        print("Total : $", sum_months, file = f)
+
+        # Calculating difference list
+        diff_list = []
+        #defining i in values list
+        for i in range(1, len(list_months_total)):
+            #calculating difference between current date and the next one
+            diff_list.append(list_months_total[i] - list_months_total[i-1])
+            #calculating minimum profit
+            min_profit_change=min(diff_list)
+            #calculating maximum profit
+            max_profit_change=max(diff_list)
+            #finding the correct date throught index match
+            max_profit_change_date = str(list_of_months[diff_list.index(max(diff_list))])
+            min_profit_change_date = str(list_of_months[diff_list.index(min(diff_list))])
+            #calculating average
+            average_change = sum(diff_list)/len(diff_list)
+            #formatting average to 2 decimals
+            format_average = "{:.2f}".format(average_change)
+        # printing difference list
+        print("Average Change: ", format_average, file = f)
+        print("Greatest Increase in Profits:", max_profit_change_date,"($", max_profit_change,")", file = f)
+        print("Greatest Decrease in Profits:", min_profit_change_date,"($", min_profit_change,")", file = f)
+
---
+++ PyPoll/main_poll.py
@@ -0,0 +1,49 @@
+#falta hacer camino cvspath y convertir en texto
+#Creating PyPoll code
+#importing module os and csv
+
+import csv
+with open("text_file.txt", "a") as f:
+    #csvpath = os.path.join('Resources','election_data.csv')
+    #opening file
+    with open('election_data.csv', encoding='UTF-8') as csvfile:
+        csvreader=csv.reader(csvfile,delimiter=',')
+        #skipping first row
+        next(csvfile)
+        votes_per_candidate = []
+        total_votes=0
+        for v in csvreader:
+            votes=v[0]
+            percandidate=v[2]
+            #Counting the amount of votes
+            total_votes+= 1
+            #Creating votes list divided by each candidate
+            votes_per_candidate.append(percandidate)
+            #counting votes per candidate
+            votes_Charles = votes_per_candidate.count('Charles Casper Stockham')
+            votes_DeGette = votes_per_candidate.count('Diana DeGette')
+            votes_Raymond = votes_per_candidate.count('Raymon Anthony Doane')
+            #calculating percentages per candidate
+            percent_Charles = votes_Charles/total_votes
+            percent_DeGette = votes_DeGette/total_votes
+            percent_Raymond = votes_Raymond/total_votes
+
+    print("Election Results", file=f)
+    print("--------------------------------", file=f)
+    print("Total Votes: " , total_votes, file=f)
+    print("--------------------------------", file=f)
+    print("Charles Casper Stockham:",percent_Charles, "% ($", votes_Charles,")", file=f)
+    print("Diana DeGette: " ,percent_DeGette, "% ($", votes_DeGette,")", file=f)
+    print("Raymon Anthony Doane: " , percent_Raymond, "% ($", votes_Raymond,")", file=f)
+    print("--------------------------------", file=f)
+    print("--------------------------------", file=f)
+    #Creating list for results, to pick the highest amount of votes
+    results=[votes_Charles,votes_DeGette,votes_Raymond]
+    winner=max(results)
+    #Creating conditionals to announce winner in a message
+    if votes_Charles>votes_Raymond and votes_Charles>votes_DeGette:
+        print("Winner : Charles Casper Stockham", file=f)
+    if votes_DeGette>votes_Raymond and votes_DeGette>votes_Charles:
+        print("Winner : Diana DeGette", file=f)
+    if votes_Raymond>votes_DeGette and votes_Raymond>votes_Charles:
+        print("Winner : Raymon Anthony Doane", file=f)
